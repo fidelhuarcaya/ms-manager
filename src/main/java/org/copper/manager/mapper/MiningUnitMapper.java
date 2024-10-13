abe046d78d8f8b53a6ae9f07764efe2e58bab301
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-10-13 11:24:02 -0500

    ms-manager v0.0.1-SNAPSHOT

--- src/main/java/org/copper/manager/mapper/MiningUnitMapper.java
+++ src/main/java/org/copper/manager/mapper/MiningUnitMapper.java
@@ -4,8 +4,9 @@ package org.copper.manager.mapper;
 import org.copper.manager.dto.request.MiningUnitRequest;
 import org.copper.manager.dto.response.MiningUnitResponse;
 import org.copper.manager.entity.MiningUnit;
+import org.mapstruct.Mapper;
 
-
+@Mapper(componentModel = "spring")
 public interface MiningUnitMapper {
 
     MiningUnit toEntity(MiningUnitRequest request);
