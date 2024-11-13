a22271431856552ddc98b763cacc51da608ebd61
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-11-12 21:52:02 -0500

    feat: add endpoint to get all courses

--- src/main/java/org/copper/manager/entity/Course.java
+++ src/main/java/org/copper/manager/entity/Course.java
@@ -17,6 +17,9 @@ public class Course extends Auditable {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "banner")
     private String banner;
 
