18a237fff951ba5769a2b13f46a7025cccc06514
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2025-01-18 23:46:35 -0500

    feat: add status at area service

--- src/main/java/org/copper/manager/repository/AreaRepository.java
+++ src/main/java/org/copper/manager/repository/AreaRepository.java
@@ -2,6 +2,8 @@ package org.copper.manager.repository;
 
 import org.copper.manager.entity.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ public interface AreaRepository extends JpaRepository<Area, Integer> {
 
     List<Area> findAllByStatusId(Integer statusId);
 
-    List<Area> findByMiningUnitId(Long miningUnitId);
+    @Query("select a from Area a where a.miningUnit.id = :miningUnitId and a.status.code = 'ACTIVE'")
+    List<Area> findByMiningUnitId(@Param("miningUnitId") Long miningUnitId);
 }
--- src/main/java/org/copper/manager/repository/TopicRepository.java
+++ src/main/java/org/copper/manager/repository/TopicRepository.java
@@ -2,6 +2,7 @@ package org.copper.manager.repository;
 
 import org.copper.manager.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import java.util.List;
 public interface TopicRepository extends JpaRepository<Topic, Long> {
     List<Topic> findAllByStatusId(@Param("statusId") Integer statusId);
 
+    @Query("select t from Topic t where t.course.id = :courseId and t.status.code = 'ACTIVE'")
     List<Topic> findTopicsByCourseId(@Param("courseId") Long id);
 }
